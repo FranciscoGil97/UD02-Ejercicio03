c6da20495a2ed4bc14eaf6eebce2d4a32f65f382
Francisco Julian Gil Marin <fj.gil16@iesdoctorbalmis.com>
2020-10-24 13:37:46 +0200

    Agregar archivos de proyecto.

---
+++ UD02-Ejercicio03/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+
+namespace UD02_Ejercicio03
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpacidadRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)altaRadioBoton.IsChecked)
+                imagen.Opacity = 1;
+            else
+                imagen.Opacity = (bool)mediaRadioBoton.IsChecked ? 0.6 : 0.3;
+        }
+
+        private void AjusteRadioBoton_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)rellenoRadioBoton.IsChecked)
+                imagen.Stretch = Stretch.Fill;
+            else if ((bool)uniformeRadioBoton.IsChecked)
+                imagen.Stretch = Stretch.Uniform;
+            else if ((bool)rellenoUniformeRadioBoton.IsChecked)
+                imagen.Stretch = Stretch.UniformToFill;
+            else
+                imagen.Stretch = Stretch.None;
+
+        }
+    }
+}
